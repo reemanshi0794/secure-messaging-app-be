6e2ffea0e18e961bfaf54d0394542a22d03183e9
Reemanshi <reema07chugh@gmail.com>
2024-08-25 18:52:06 +0530

    fix: handled if user already exist

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,14 +6,24 @@ const router = express.Router();
 
 router.post("/signup", async (req, res) => {
   const { name, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
+
   try {
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+      console.log("existingUserexistingUser",existingUser)
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await User.create({ name, email, password: hashedPassword });
     res.status(201).json({ user });
   } catch (error) {
-    res.status(400).json({ error: "Error creating user" });
+    console.log("Error creating user:", error);
+    res.status(500).json({ error: "Error creating user" });
   }
 });
+
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
